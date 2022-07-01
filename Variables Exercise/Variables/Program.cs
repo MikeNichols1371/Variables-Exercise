91c6ae68ccb17152763efa266103735c976fdfbd
MikeNichols1371 <mnich13711@gmail.com>
2022-07-01 14:40:14 -0400

    Finished Variables Exercise

---
+++ Variables Exercise/Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿string name = "Mike";
+int age = 25;
+char a = 'a';
+bool b = true;
+double weight = 90.5;
+decimal dec = 13.2m;
+
+Console.WriteLine($"Hi my name is {name}. I am {age} years old. I have {a} dog. This is a {b} statement. She weighs {weight} lbs.");
+Console.WriteLine($"My value for data type decimal is {dec}");
